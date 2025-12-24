659f5d49f2edfe81674301e3a84753e162d25595
MaximeHoullierFR <maxou.houllier@gmail.com>
2025-12-24 15:27:04 +0100

    Add MainActivity with a greeting TextView

---
+++ app/src/main/java/com/maximehoullier/myapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.maximehoullier.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "Hello, MyApp!"
+        tv.textSize = 24f
+        setContentView(tv)
+    }
+}
